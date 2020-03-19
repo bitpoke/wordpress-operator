b606e2222741b988a9a93e91d5a9932d7833e95a
Calin Don <calin.don@gmail.com>
2020-03-20 01:06:00 +0200

    Show wp-cron status to kubectl

--- pkg/apis/wordpress/v1alpha1/wordpress_types.go
+++ pkg/apis/wordpress/v1alpha1/wordpress_types.go
@@ -317,6 +317,7 @@ type WordpressStatus struct {
 // +kubebuilder:resource:shortName=wp
 // +kubebuilder:subresource:status
 // +kubebuilder:subresource:scale:specpath=.spec.replicas,statuspath=.status.replicas
+// +kubebuilder:printcolumn:name="wp-cron",type="string",JSONPath=".status.conditions[?(@.type == "WPCronTriggering")].status",description="wp-cron triggering status"
 type Wordpress struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
