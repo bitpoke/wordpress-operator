ea21050f695dfa3bdcc3e484058960ea070f0f0b
amecea <flavius@presslabs.com>
2020-05-29 20:01:03 +0300

    Cleanup pkg/apis annotations

--- pkg/apis/wordpress/v1alpha1/register.go
+++ pkg/apis/wordpress/v1alpha1/register.go
@@ -16,12 +16,6 @@ limitations under the License.
 
 // NOTE: Boilerplate only.  Ignore this file.
 
-// Package v1alpha1 contains API Schema definitions for the wordpress v1alpha1 API group
-// +k8s:openapi-gen=true
-// +k8s:deepcopy-gen=package,register
-// +k8s:conversion-gen=github.com/presslabs/wordpress-operator/pkg/apis/wordpress
-// +k8s:defaulter-gen=TypeMeta
-// +groupName=wordpress.presslabs.org
 package v1alpha1
 
 import (
@@ -35,4 +29,7 @@ var (
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
 	SchemeBuilder = &scheme.Builder{GroupVersion: SchemeGroupVersion}
+
+	// AddToScheme adds the types in this group-version to the given scheme.
+	AddToScheme = SchemeBuilder.AddToScheme
 )
