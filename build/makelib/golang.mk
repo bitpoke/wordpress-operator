b50ee8ad1acc8a8b5225f3f4dbf776b9e5bd8828
Robert Stefan <rstefan1@users.noreply.github.com>
2021-03-05 15:29:37 +0200

    Bump golang version to 1.16

--- build/makelib/golang.mk
+++ build/makelib/golang.mk
@@ -58,7 +58,7 @@ GO_LDFLAGS += -s -w
 endif
 
 # supported go versions
-GO_SUPPORTED_VERSIONS ?= 1.11|1.12|1.13|1.14|1.15
+GO_SUPPORTED_VERSIONS ?= 1.11|1.12|1.13|1.14|1.15|1.16
 
 # set GOOS and GOARCH
 GOOS := $(OS)
